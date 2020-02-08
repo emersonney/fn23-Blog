32d4f71e2ed33b8306a2ce3ef37384d1a78346ea
emerson.farias@gmail.com <emerson.farias@gmail.com>
2020-02-08 14:29:44 -0300

    Metodo de Acionar via post para o formulário e exibir na lista

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -9,22 +9,42 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        public IActionResult Index()
+
+        private IList<Post> listaDePosts;
+
+        public PostController()
         {
+            this.listaDePosts = new List<Post>
+            {
+                new Post { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+                new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+                new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
 
-           
-            var listaDePosts = new List<Post>() {
-                new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
-                new Post() { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
-                new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
-                new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
             };
+        }
 
-           
 
+    public IActionResult Index()
+        {
             //ViewBag.Posts = listaDePosts;
-
             return View(listaDePosts);
         }
+
+        public IActionResult Adiciona()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adiciona(Post post)
+        {
+            listaDePosts.Add(post);
+            return View("Index", listaDePosts);
+        }
+
     }
+
+ 
+
 }
